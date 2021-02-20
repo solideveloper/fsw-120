9ea4396f6ce0fa16aec8ac6c5807878b6a468873
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-02-19 22:00:52 -0500

    test

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+return (
+    <div>
+        <p> list of items to be checked</p>
+        <input type="checkbox" />firt item<br/>
+        <input type="checkbox" />second item<br/>
+        <input type="checkbox" />third item<br/>
+    </div>
+)
+
+}
+
+export default App
