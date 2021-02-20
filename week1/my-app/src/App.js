56b0b0b75bfde476bd292ea60e748ccba3a43185
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-02-20 15:57:39 -0500

    test

---
+++ week1/my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> This is My To Do List
+        <input type="checkbox" />firt thing to do<br/>
+        <input type="checkbox" />second thing to do<br/>
+        <input type="checkbox" />third thing to do<br/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
