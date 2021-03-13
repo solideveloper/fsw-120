4ff649c1fe720cd6ccb06544187bf2a62dc12e6a
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-03-12 21:46:50 -0500

    firstcommit

---
+++ week4/entry/src/name-entries/NameCard.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+const NameCard = ({firstName,lastName}) => {
+
+    return (
+        <div>
+            <li>
+            {firstName} {lastName}
+            </li>
+        </div>
+    );
+}
+
+export default NameCard;
