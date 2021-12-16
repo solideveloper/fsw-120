b168eb81c3df5d3c6bb707e52cf112a989a8e06d
solideveloper <stephanie.olivares@bryanuniversity.edu>
2021-12-15 23:15:31 -0500

    test

--- week7/mocktwitter/src/components/Navbar/Navbar.js
+++ week7/mocktwitter/src/components/Navbar/Navbar.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { withAuth } from '../../providers/AuthProvider';
 import './navbar.css';
 
 const Navbar = () => {
 
-  return (
-    <div>
-      <ul className='navbarContainer'>
-        <Link to='/' className='links'> Home </Link>
-        <Link to='/tweets' className='links'> Tweets </Link> 
-        <Link to='/login' className='links'> Log in </Link>
-      </ul>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        ul className = 'navbarContainer' >
+        <
+        BrowserRouter >
+        <
+        Link to = '/'
+        className = 'links' > Home < /Link> <
+        Link to = '/tweets'
+        className = 'links' > Tweets < /Link>  <
+        Link to = '/login'
+        className = 'links' > Log in < /Link> <
+        /BrowserRouter> <
+        /ul> <
+        /div>
+    );
 }
 
 export default withAuth(Navbar);
