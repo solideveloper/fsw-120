8b34e99728125e6bd825284fa4b8fb1724847389
solideveloper <stephanie.olivares@bryanuniversity.edu>
2021-12-16 10:37:57 -0500

    test

--- week7/mocktwitter/src/App.js
+++ week7/mocktwitter/src/App.js
@@ -9,45 +9,37 @@ import Login from './components/Login/Login';
 
 const App = () => {
 
-    return ( <
-        div >
+    return ( 
+    <div>
+        <Navbar/>
+        <Routes>
+            <Route 
+                path = "/home"
+                element = { < Home / > }/> 
+            <Route 
+                path = "/login"
+                element = { < Login / > }/> 
+            <Route 
+                path = "/tweets"
+                element = { < TweetPost / > }/> 
+        </Routes>
 
-        <
-        Navbar / >
-        <
-        Routes >
-        <
-        Route path = "/home"
-        element = { < Home / > }
-        /> <
-        Route path = "/login"
-        element = { < Login / > }
-        /> <
-        Route path = "/tweets"
-        element = { < TweetPost / > }
-        /> <
-        /Routes>
+        <div className = "footer-container" >
 
-        <
-        div className = "footer-container" >
-
-        <
-        img className = "footerimg"
-        src = "https://user-images.githubusercontent.com/72266712/111890201-9fef0c80-89bd-11eb-8e84-ff06eeaed2d0.png"
-        alt = "facebook" / >
-        <
-        img className = "footerimg"
-        src = "https://user-images.githubusercontent.com/72266712/111890202-9fef0c80-89bd-11eb-9b7e-be65cf93ba6f.png"
-        alt = "instagram" / >
-        <
-        img className = "footerimg"
-        src = "https://user-images.githubusercontent.com/72266712/111890203-9fef0c80-89bd-11eb-904a-986da2a4fc54.png"
-        alt = "twitter icon" / >
-        <
-        br / >
-        &
-        copy; 2021 - Stephanie Olivares < /div> <
-        /div>
+            <img className = "footerimg"
+            src = "https://user-images.githubusercontent.com/72266712/111890201-9fef0c80-89bd-11eb-8e84-ff06eeaed2d0.png"
+            alt = "facebook" / >
+            <img className = "footerimg"
+            src = "https://user-images.githubusercontent.com/72266712/111890202-9fef0c80-89bd-11eb-9b7e-be65cf93ba6f.png"
+            alt = "instagram" / >
+            <img className = "footerimg"
+            src = "https://user-images.githubusercontent.com/72266712/111890203-9fef0c80-89bd-11eb-904a-986da2a4fc54.png"
+            alt = "twitter icon" / >
+            <br / >
+            &
+            copy; 2021 - Stephanie Olivares 
+        </div> 
+    </div>
 
     );
 }
--- week7/mocktwitter/src/components/Navbar/Navbar.js
+++ week7/mocktwitter/src/components/Navbar/Navbar.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { withAuth } from '../../providers/AuthProvider';
 import './navbar.css';
 
 const Navbar = () => {
 
-    return ( <
-        div >
-        <
-        ul className = 'navbarContainer' >
-        <
-        BrowserRouter >
-        <
-        Link to = '/'
-        className = 'links' > Home < /Link> <
-        Link to = '/tweets'
-        className = 'links' > Tweets < /Link>  <
-        Link to = '/login'
-        className = 'links' > Log in < /Link> <
-        /BrowserRouter> <
-        /ul> <
-        /div>
+    return ( 
+    <div>
+        <ul className = 'navbarContainer'>
+            <Link to = '/home' className = 'links'> Home </Link> 
+            <Link to = '/tweets' className = 'links'> Tweets </Link>  
+            <Link to = '/login' className = 'links'> Log in </Link> 
+        </ul> 
+    </div>
     );
 }
 
